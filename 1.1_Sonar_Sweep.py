4cebe41110e9b9cb031da3c65d87e0b7da39e050
mekan841 <apamekhail@gmail.com>
2021-12-02 18:31:21 +0000

    Update 1.1_Sonar_Sweep.py

--- 1.1_Sonar_Sweep.py
+++ 1.1_Sonar_Sweep.py
@@ -1,3 +1,5 @@
+#First Puzzle
+
 import numpy as np
 import pandas as pd
 
@@ -11,3 +13,16 @@ for d in differences:
     else: change.append('decrease')
 
 change.count('increase')
+
+
+#Second Puzzle
+triple_sums = [depths[i] + depths[i+1] + depths[i+2] for i in range(len(depths)-2)]
+
+triple_differences = np.diff(triple_sums)
+triple_change = []
+
+for d in triple_differences:
+    if d > 0: triple_change.append('increase')
+    else: triple_change.append('decrease')
+
+triple_change.count('increase')
