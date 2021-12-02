c4b67fe9d565200e654110435e8e33caf8afffb7
mekan841 <apamekhail@gmail.com>
2021-12-02 18:48:08 +0000

    Create 2_Dive.py

---
+++ 2_Dive.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np
+
+dive_example = pd.read_csv("/Users/anni/Desktop/dive_example.csv")
+dive_grouped = dive_example.groupby('Direction')['Metres'].sum()
+
+dive_grouped['up'] = dive_grouped['up']*-1
+
+vertical_distance = dive_grouped['up']+dive_grouped['down']
+
+multiplier = dive_grouped['forward']*vertical_distance
+print (multiplier)
